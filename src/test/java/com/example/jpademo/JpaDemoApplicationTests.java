80b55b0afdc15023e845e100b76020fcd8f8367a
Nathaniel-Ace <nathanielacep@gmail.com>
2024-03-23 12:12:52 +0100

    Tour backend implemented

--- src/test/java/com/example/jpademo/JpaDemoApplicationTests.java
+++ src/test/java/com/example/jpademo/JpaDemoApplicationTests.java
@@ -2,15 +2,14 @@ package com.example.jpademo;
 
 import com.example.jpademo.persistence.entities.AddressEntity;
 import com.example.jpademo.persistence.entities.PersonEntity;
+import com.example.jpademo.persistence.entities.TourEntity;
 import com.example.jpademo.persistence.repositories.AddressRepository;
 import com.example.jpademo.persistence.repositories.PersonRepository;
+import com.example.jpademo.persistence.repositories.TourRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +19,8 @@ class JpaDemoApplicationTests {
     private PersonRepository personRepository;
     @Autowired
     private AddressRepository addressRepository;
+    @Autowired
+    private TourRepository tourRepository;
 
     @Test
     void contextLoads() {
@@ -92,6 +93,36 @@ class JpaDemoApplicationTests {
 
         System.out.println("find addresses by person:");
         addressRepository.findByPerson(anna).forEach(System.out::println);
+    }
+
+    @Test
+    void test_TourEntity() {
+        TourEntity tour1 = TourEntity.builder()
+                .name("Tour 1")
+                .description("Description 1")
+                .from_location("Location 1")
+                .to_location("Location 2")
+                .transport_type("Bus")
+                .distance(100)
+                .time(2)
+                .route_info("Route 1")
+                .build();
+
+        tourRepository.save(tour1);
+
+        tourRepository.save(TourEntity.builder()
+                .name("Tour 2")
+                .description("Description 2")
+                .from_location("Location 3")
+                .to_location("Location 4")
+                .transport_type("Train")
+                .distance(200)
+                .time(3)
+                .route_info("Route 2")
+                .build());
+
+        System.out.printf("%d rows in table tour\n", tourRepository.count());
+        tourRepository.findAll().forEach(System.out::println);
     }
 
 }
