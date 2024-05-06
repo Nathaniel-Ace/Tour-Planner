dee38fb9a893f175617e9a383378b449dec98195
e6mr6e <147497852+e6mr6e@users.noreply.github.com>
2024-05-07 01:02:23 +0200

    APPBAR ZU VIELE ERRORS GEHABT UND ZEIT VERSCHWENDET!!!!

--- tour-planner-frontend/src/App.js
+++ tour-planner-frontend/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import AppBarComponent from './components/Appbar'; // Hier ist der Pfad und Dateiname korrigiert
 
 function App() {
-  return (
-    <div className="App">
-     TEST APP
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBarComponent/> {/* Name der Komponente aktualisiert */}
+        </div>
+    );
 }
 
 export default App;
